6b9aba89be5827b302985b038acc940a3521878e
Shashank <shashank_smaty@yahoo.co.in>
2018-01-29 23:57:49 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+// mongodb connection
+mongoose.connect('mongodb://localhost/makenote-db').then(() => {
+  console.log("MongoDb is connected...");
+}).catch((err) => {
+  console.log(err);
+});
+
+// load notes model
+require('./models/Notes');
+const Notes = mongoose.model('notes');
+
+// Template Engine
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes - Index page
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "MakeNote"
+  });
+});
+
+// Routes - About page
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About"
+  });
+});
+
+// Routes - Add page
+app.get('/notes/create', (req, res) => {
+  res.render('notes/create', {
+    title: 'Create Note'
+  });
+});
+
+// post - create a note
+app.post('/create', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ models/Notes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create schema
+const NoteSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// create model
+mongoose.model('notes', NoteSchema);
